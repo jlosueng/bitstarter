86367f09f1687724704b677a926e693898105f09
Joe Lofshult <jlofshult@gmail.com>
2013-07-09 12:30:54 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+
+fs.readFile('index.html', 'utf8' function(err, data) {
+   if (err) {
+      return console.log(err);
+   }
+   results = data.toString();
+});
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(results);
 });
 
 var port = process.env.PORT || 5000;
